2f421badccd11db9adff9f6a2bd9885dce066075
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-07 17:41:14 -0400

    avx2: fix reduce in gf2x.c

--- hqc/patches/Optimized_Implementation_hqc-rmrs-192_src_gf2x.c
+++ hqc/patches/Optimized_Implementation_hqc-rmrs-192_src_gf2x.c
@@ -1,50 +1,170 @@
 --- hqc-2020-05-29/Optimized_Implementation/hqc-rmrs-192/src/gf2x.c
 +++ hqc-2020-05-29-patched/Optimized_Implementation/hqc-rmrs-192/src/gf2x.c
-@@ -17,14 +17,14 @@
- #define VEC_N_ARRAY_SIZE_VEC CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
- #define WORD 64
- #define LAST64 (PARAM_N >> 6)
+@@ -14,26 +14,22 @@
+ #define T_TM3_3W_256 64
+ #define T_TM3_3W_64 256
+ 
+-#define VEC_N_ARRAY_SIZE_VEC CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
+-#define WORD 64
+-#define LAST64 (PARAM_N >> 6)
 -uint64_t a1_times_a2[VEC_N_256_SIZE_64 << 1];
-+uint64_t a1_times_a2[2 * VEC_N_256_SIZE_64 + 1];
- uint64_t tmp_reduce[VEC_N_ARRAY_SIZE_VEC << 2];
- __m256i *o256 = (__m256i *) tmp_reduce;
+-uint64_t tmp_reduce[VEC_N_ARRAY_SIZE_VEC << 2];
+-__m256i *o256 = (__m256i *) tmp_reduce;
++#define VEC_N_SIZE_256 CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
++__m256i a1_times_a2[2 * VEC_N_SIZE_256 + 1];
  uint64_t bloc64[PARAM_OMEGA_R]; // Allocation with the biggest possible weight
  uint64_t bit64[PARAM_OMEGA_R]; // Allocation with the biggest possible weight
  
  
 -static inline void reduce(uint64_t *o, uint64_t *a);
+-inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
+-inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_8(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_16(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_64(__m256i *C, __m256i *A, __m256i *B);
 +static inline void reduce(uint64_t *o, const uint64_t *a);
- inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
- inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
- inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
-@@ -33,7 +33,7 @@
- inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
- inline static void karat_mult_64(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
++static inline void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_8(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_16(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_64(__m256i *C, __m256i *A, __m256i *B);
  static inline void divByXplus1(__m256i *out,__m256i *in,int size);
 -void TOOM3Mult(uint64_t* Out, const uint64_t *A, const uint64_t *B);
-+static void TOOM3Mult(uint64_t* Out, const uint64_t *A, const uint64_t *B);
++static void TOOM3Mult(__m256i *Out, const uint64_t *A, const uint64_t *B);
  
  
  
-@@ -45,7 +45,7 @@
+@@ -45,43 +41,18 @@
   * @param[out] o Pointer to the result
   * @param[in] a Pointer to the polynomial a(x)
   */
 -static inline void reduce(uint64_t *o, uint64_t *a) {
+-	__m256i r256, carry256;
+-	__m256i *a256 = (__m256i *) a;
+-	static const int32_t dec64 = PARAM_N & 0x3f;
+-	static const int32_t d0 = WORD - dec64;
+-	int32_t i, i2;
+-
+-	for (i = LAST64 ; i < (PARAM_N >> 5) - 4 ; i += 4) {
+-		r256 = _mm256_lddqu_si256((__m256i const *) (& a[i]));
+-		r256 = _mm256_srli_epi64(r256, dec64);
+-		carry256 = _mm256_lddqu_si256((__m256i const *) (& a[i + 1]));
+-		carry256 = _mm256_slli_epi64(carry256, d0);
+-		r256 ^= carry256;
+-		i2 = (i - LAST64) >> 2;
+-		o256[i2] = a256[i2] ^ r256;
+-	}
+-
+-    r256 = _mm256_lddqu_si256((__m256i const *)(& a[i]));
+-	r256 = _mm256_srli_epi64(r256, dec64);
+-	carry256 = _mm256_lddqu_si256((__m256i const *)(& a[i + 1]));
+-	carry256 = _mm256_slli_epi64(carry256, d0);
+-	r256 ^= carry256;
+-	i2 =(i - LAST64) >> 2;
+-	o256[i2] = a256[i2] ^ r256;
+-	i += 4;
+-	r256 = _mm256_lddqu_si256((__m256i const *)(& a[i]));
+-	carry256 = _mm256_lddqu_si256((__m256i const *) (& a[i + 1]));
+-	r256 = _mm256_srli_epi64(r256, dec64);
+-	carry256 = _mm256_slli_epi64(carry256, d0);
+-	r256 ^= carry256;
+-	i2 = (i - LAST64) >> 2;
+-	o256[i2] = (a256[i2] ^ r256);
+-	tmp_reduce[LAST64] &= RED_MASK;
+-	memcpy(o, tmp_reduce, VEC_N_SIZE_BYTES);
+-}
 +static inline void reduce(uint64_t *o, const uint64_t *a) {
- 	__m256i r256, carry256;
- 	__m256i *a256 = (__m256i *) a;
- 	static const int32_t dec64 = PARAM_N & 0x3f;
-@@ -392,7 +392,7 @@
++	uint64_t r;
++	uint64_t carry;
+ 
++	for (uint32_t i = 0 ; i < VEC_N_SIZE_64 ; i++) {
++		r = a[i + VEC_N_SIZE_64 - 1] >> (PARAM_N & 63);
++		carry = (uint64_t) (a[i + VEC_N_SIZE_64] << (64 - (PARAM_N & 63)));
++		o[i] = a[i] ^ r ^ carry;
++	}
+ 
++	o[VEC_N_SIZE_64 - 1] &= RED_MASK;
++}
+ 
+ /**
+  * @brief Compute C(x) = A(x)*B(x)
+@@ -92,7 +63,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B) {
++static inline void karat_mult_1(__m128i *C, __m128i *A, __m128i *B) {
+ 	__m128i D1[2];
+ 	__m128i D0[2], D2[2];
+ 	__m128i Al = _mm_loadu_si128(A);
+@@ -149,7 +120,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_2(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[2],D1[2],D2[2],SAA,SBB;
+ 	__m128i *A128 = (__m128i *)A, *B128 = (__m128i *)B;
+ 
+@@ -179,7 +150,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_4(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[4],D1[4],D2[4],SAA[2],SBB[2];
+ 
+ 	karat_mult_2( D0, A,B);
+@@ -216,7 +187,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_8(__m256i * C, __m256i * A, __m256i * B) {
++static inline void karat_mult_8(__m256i * C, __m256i * A, __m256i * B) {
+ 	__m256i D0[8],D1[8],D2[8],SAA[4],SBB[4];
+ 
+ 	karat_mult_4( D0, A,B);
+@@ -255,7 +226,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_16(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_16(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[16],D1[16],D2[16],SAA[8],SBB[8];
+ 
+ 	karat_mult_8( D0, A,B);
+@@ -294,7 +265,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_32(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[32],D1[32],D2[32],SAA[16],SBB[16];
+ 
+ 	karat_mult_16( D0, A, B);
+@@ -333,7 +304,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_64(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_64(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[64],D1[64],D2[64],SAA[32],SBB[32];
+ 
+ 	karat_mult_32( D0, A, B);
+@@ -392,7 +363,7 @@
   * @param[in] A Pointer to the polynomial A(x)
   * @param[in] B Pointer to the polynomial B(x)
   */
 -void TOOM3Mult(uint64_t* Out, const uint64_t* A, const uint64_t* B) {
-+static void TOOM3Mult(uint64_t* Out, const uint64_t* A, const uint64_t* B) {
++static void TOOM3Mult(__m256i *Out, const uint64_t* A, const uint64_t* B) {
  	static __m256i U0[T_TM3_3W_256], V0[T_TM3_3W_256], U1[T_TM3_3W_256], V1[T_TM3_3W_256], U2[T_TM3_3W_256], V2[T_TM3_3W_256];
  	static __m256i W0[2 * (T_TM3_3W_256)], W1[2 * (T_TM3_3W_256)], W2[2 * (T_TM3_3W_256)], W3[2 * (T_TM3_3W_256)], W4[2 * (T_TM3_3W_256)];
  	static __m256i tmp[2 * (T_TM3_3W_256)];
-@@ -433,30 +433,29 @@
+@@ -433,30 +404,29 @@
  
  	//W1 = W2 * W3
  	karat_mult_64( W1, W2, W3);
@@ -89,7 +209,7 @@
  	}
  
  	//W3 = W3 + W0      ; W2 = W2 + W4
-@@ -471,7 +470,6 @@
+@@ -471,7 +441,6 @@
  		W4[i] ^= V0[i];
  	}
  
@@ -97,7 +217,7 @@
  	karat_mult_64(tmp, W3, W2);
  
  	for (int32_t i = 0 ; i < 2 * (T_TM3_3W_256) ; i++) {
-@@ -497,35 +495,35 @@
+@@ -497,35 +466,35 @@
  	}
  
  	//W2 =(W2 + W0)/x -> x = X^64
@@ -142,7 +262,7 @@
  	}
  
  	divByXplus1(W3,tmp,T_TM3_3W_256);
-@@ -554,15 +552,15 @@
+@@ -554,19 +523,19 @@
  	ro256[(T_TM3_3W_256 << 2) - 2] = W2[(T_TM3_3W_256 << 1) - 1] ^ W4[0];
  	ro256[(T_TM3_3W_256 * 6) - 3] = W4[(T_TM3_3W_256 << 1) - 1];
  
@@ -162,4 +282,28 @@
  	}
  
  	for (int32_t i = 0 ; i < 6 * T_TM3_3W_256 - 2 ; i++) {
+-		uint64_t *out64 = Out + (i << 2);
++		uint64_t *out64 = ((uint64_t *)Out) + (i << 2);
+ 		_mm256_storeu_si256((__m256i *)out64, ro256[i]);
+ 	}
+ }
+@@ -585,12 +554,12 @@
+  */
+ void vect_mul(uint64_t *o, const uint64_t *a1, const uint64_t *a2) {
+ 	TOOM3Mult(a1_times_a2, a1, a2);
+-	reduce(o, a1_times_a2);
++	reduce(o, (uint64_t *)a1_times_a2);
+ 
+ 	// clear all
+-	#ifdef __STDC_LIB_EXT1__
+-		memset_s(a1_times_a2, 0, (VEC_N_SIZE_64 << 1) * sizeof(uint64_t));
+-	#else
+-		memset(a1_times_a2, 0, (VEC_N_SIZE_64 << 1) * sizeof(uint64_t));
+-	#endif
++#ifdef __STDC_LIB_EXT1__
++	memset_s(a1_times_a2, 0, (2 * VEC_N_SIZE_256 + 1) * sizeof(__m256i));
++#else
++	memset(a1_times_a2, 0, (2 * VEC_N_SIZE_256 + 1) * sizeof(__m256i));
++#endif
+ }
 
--- hqc/patches/Optimized_Implementation_hqc-rmrs-256_src_gf2x.c
+++ hqc/patches/Optimized_Implementation_hqc-rmrs-256_src_gf2x.c
@@ -1,69 +1,154 @@
 --- hqc-2020-05-29/Optimized_Implementation/hqc-rmrs-256/src/gf2x.c
 +++ hqc-2020-05-29-patched/Optimized_Implementation/hqc-rmrs-256/src/gf2x.c
-@@ -21,14 +21,14 @@
- #define VEC_N_ARRAY_SIZE_VEC CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
- #define WORD 64
- #define LAST64 (PARAM_N >> 6)
+@@ -18,27 +18,23 @@
+ #define T_TM3R_3W_256 93
+ #define T_TM3R_3W_64 (T_TM3R_3W_256<<2)
+ 
+-#define VEC_N_ARRAY_SIZE_VEC CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
+-#define WORD 64
+-#define LAST64 (PARAM_N >> 6)
 -uint64_t a1_times_a2[VEC_N_256_SIZE_64 << 1];
-+uint64_t a1_times_a2[2*VEC_N_256_SIZE_64 + 1];
- uint64_t tmp_reduce[VEC_N_ARRAY_SIZE_VEC << 2];
- __m256i *o256 = (__m256i *) tmp_reduce;
+-uint64_t tmp_reduce[VEC_N_ARRAY_SIZE_VEC << 2];
+-__m256i *o256 = (__m256i *) tmp_reduce;
++#define VEC_N_SIZE_256 CEIL_DIVIDE(PARAM_N, 256) /*!< The number of needed vectors to store PARAM_N bits*/
++__m256i a1_times_a2[2 * VEC_N_SIZE_256 + 1];
  uint64_t bloc64[PARAM_OMEGA_R]; // Allocation with the biggest possible weight
  uint64_t bit64[PARAM_OMEGA_R]; // Allocation with the biggest possible weight
  
  
 -static inline void reduce(uint64_t *o, uint64_t *a);
+-inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
+-inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_8(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_16(__m256i *C, __m256i *A, __m256i *B);
+-inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
 +static inline void reduce(uint64_t *o, const uint64_t *a);
- inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
- inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
- inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
-@@ -37,8 +37,8 @@
- inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_1(__m128i *C, __m128i *A, __m128i *B);
++static inline void karat_mult_2(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_4(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_8(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_16(__m256i *C, __m256i *A, __m256i *B);
++static inline void karat_mult_32(__m256i *C, __m256i *A, __m256i *B);
  static inline void divByXplus1(__m256i *out,__m256i *in,int size);
  static inline void divByXplus1_256(__m256i *out,__m256i *in,int size);
 -void TOOM3Mult(uint64_t* Out, const uint64_t *A, const uint64_t *B);
 -void TOOM3RecMult(uint64_t *Out, const uint64_t *A, const uint64_t *B);
-+static void TOOM3Mult(uint64_t* Out, const uint64_t *A, const uint64_t *B);
-+static void TOOM3RecMult(uint64_t *Out, const uint64_t *A, const uint64_t *B);
++static void TOOM3Mult(__m256i *Out, const uint64_t *A, const uint64_t *B);
++static void TOOM3RecMult(__m256i *Out, const uint64_t *A, const uint64_t *B);
  
  
  
-@@ -50,7 +50,7 @@
+@@ -50,35 +46,18 @@
   * @param[out] o Pointer to the result
   * @param[in] a Pointer to the polynomial a(x)
   */
 -static inline void reduce(uint64_t *o, uint64_t *a) {
-+static inline void reduce(uint64_t *o, const uint64_t *a) {
- 	__m256i r256, carry256;
- 	__m256i *a256 = (__m256i *) a;
- 	static const int32_t dec64 = PARAM_N & 0x3f;
-@@ -67,13 +67,13 @@
- 		o256[i2] = a256[i2] ^ r256;
- 	}
- 
+-	__m256i r256, carry256;
+-	__m256i *a256 = (__m256i *) a;
+-	static const int32_t dec64 = PARAM_N & 0x3f;
+-	static const int32_t d0 = WORD - dec64;
+-	int32_t i, i2;
+-
+-	for (i = LAST64 ; i < (PARAM_N >> 5) - 4 ; i += 4) {
+-		r256 = _mm256_lddqu_si256((__m256i const *) (& a[i]));
+-		r256 = _mm256_srli_epi64(r256, dec64);
+-		carry256 = _mm256_lddqu_si256((__m256i const *) (& a[i + 1]));
+-		carry256 = _mm256_slli_epi64(carry256, d0);
+-		r256 ^= carry256;
+-		i2 = (i - LAST64) >> 2;
+-		o256[i2] = a256[i2] ^ r256;
+-	}
+-
 -    r256 = _mm256_lddqu_si256((__m256i const *)(& a[i]));
 -	carry256 = _mm256_lddqu_si256((__m256i const *) (& a[i + 1]));
-+	r256 = _mm256_lddqu_si256((__m256i const *)(& a[i]));
-+	carry256 = _mm256_lddqu_si256((__m256i const *)(& a[i + 1]));
- 	r256 = _mm256_srli_epi64(r256, dec64);
- 	carry256 = _mm256_slli_epi64(carry256, d0);
- 	r256 ^= carry256;
- 	i2 = (i - LAST64) >> 2;
+-	r256 = _mm256_srli_epi64(r256, dec64);
+-	carry256 = _mm256_slli_epi64(carry256, d0);
+-	r256 ^= carry256;
+-	i2 = (i - LAST64) >> 2;
 -	o256[i2] = (a256[i2] ^ r256);
-+	o256[i2] = a256[i2] ^ r256;
- 	tmp_reduce[LAST64] &= RED_MASK;
- 	memcpy(o, tmp_reduce, VEC_N_SIZE_BYTES);
- }
-@@ -351,7 +351,7 @@
+-	tmp_reduce[LAST64] &= RED_MASK;
+-	memcpy(o, tmp_reduce, VEC_N_SIZE_BYTES);
+-}
++static inline void reduce(uint64_t *o, const uint64_t *a) {
++	uint64_t r;
++	uint64_t carry;
+ 
++	for (uint32_t i = 0 ; i < VEC_N_SIZE_64 ; i++) {
++		r = a[i + VEC_N_SIZE_64 - 1] >> (PARAM_N & 63);
++		carry = (uint64_t) (a[i + VEC_N_SIZE_64] << (64 - (PARAM_N & 63)));
++		o[i] = a[i] ^ r ^ carry;
++	}
+ 
++	o[VEC_N_SIZE_64 - 1] &= RED_MASK;
++}
+ 
+ /**
+  * @brief Compute C(x) = A(x)*B(x)
+@@ -89,7 +68,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_1(__m128i *C, __m128i *A, __m128i *B) {
++static inline void karat_mult_1(__m128i *C, __m128i *A, __m128i *B) {
+ 	__m128i D1[2];
+ 	__m128i D0[2], D2[2];
+ 	__m128i Al = _mm_loadu_si128(A);
+@@ -146,7 +125,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_2(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_2(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[2],D1[2],D2[2],SAA,SBB;
+ 	__m128i *A128 = (__m128i *)A, *B128 = (__m128i *)B;
+ 
+@@ -176,7 +155,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_4(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_4(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[4],D1[4],D2[4],SAA[2],SBB[2];
+ 
+ 	karat_mult_2( D0, A,B);
+@@ -213,7 +192,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_8(__m256i * C, __m256i * A, __m256i * B) {
++static inline void karat_mult_8(__m256i * C, __m256i * A, __m256i * B) {
+ 	__m256i D0[8],D1[8],D2[8],SAA[4],SBB[4];
+ 
+ 	karat_mult_4( D0, A,B);
+@@ -252,7 +231,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_16(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_16(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[16],D1[16],D2[16],SAA[8],SBB[8];
+ 
+ 	karat_mult_8( D0, A,B);
+@@ -291,7 +270,7 @@
+  * @param[in] A Pointer to the polynomial A(x)
+  * @param[in] B Pointer to the polynomial B(x)
+  */
+-inline static void karat_mult_32(__m256i *C, __m256i *A, __m256i *B) {
++static inline void karat_mult_32(__m256i *C, __m256i *A, __m256i *B) {
+ 	__m256i D0[32],D1[32],D2[32],SAA[16],SBB[16];
+ 
+ 	karat_mult_16( D0, A, B);
+@@ -351,7 +330,7 @@
   * @param[in] A Pointer to the polynomial A(x)
   * @param[in] B Pointer to the polynomial B(x)
   */
 -void TOOM3Mult(uint64_t* Out, const uint64_t* A, const uint64_t* B) {
-+static void TOOM3Mult(uint64_t *Out, const uint64_t* A, const uint64_t* B) {
++static void TOOM3Mult(__m256i *Out, const uint64_t* A, const uint64_t* B) {
  	static __m256i U0[T_TM3_3W_256], V0[T_TM3_3W_256], U1[T_TM3_3W_256], V1[T_TM3_3W_256], U2[T_TM3_3W_256], V2[T_TM3_3W_256];
  	static __m256i W0[2 * (T_TM3_3W_256)], W1[2 * (T_TM3_3W_256)], W2[2 * (T_TM3_3W_256)], W3[2 * (T_TM3_3W_256)], W4[2 * (T_TM3_3W_256)];
  	static __m256i tmp[2 * (T_TM3_3W_256)];
-@@ -394,28 +394,28 @@
+@@ -394,28 +373,28 @@
  	karat_mult_32( W1, W2, W3);
  
  	//W0 =(U1 + U2*x)*x ; W4 =(V1 + V2*x)*x (SIZE = T_TM3_3W_256 !)
@@ -106,7 +191,7 @@
  	}
  
  	//W3 = W3 + W0      ; W2 = W2 + W4
-@@ -455,35 +455,35 @@
+@@ -455,35 +434,35 @@
  	}
  
  	//W2 =(W2 + W0)/x -> x = X^64
@@ -151,7 +236,7 @@
  	}
  
  	divByXplus1(W3,tmp,T_TM3_3W_256);
-@@ -502,7 +502,6 @@
+@@ -502,7 +481,6 @@
  	// Recomposition
  	//W  = W0+ W1*x+ W2*x^2+ W3*x^3 + W4*x^4
  	//W0, W1, W4 of size 2*T_TM3_3W_256, W2 and W3 of size 2*(T_TM3_3W_256)
@@ -159,7 +244,7 @@
  	for (int32_t i = 0 ; i < (T_TM3_3W_256 << 1) - 1 ; i++) {
  		ro256[i] = W0[i];
  		ro256[i + 2 * T_TM3_3W_256 - 1] = W2[i];
-@@ -513,15 +512,15 @@
+@@ -513,19 +491,19 @@
  	ro256[(T_TM3_3W_256 << 2) - 2] = W2[(T_TM3_3W_256 << 1) - 1] ^ W4[0];
  	ro256[(T_TM3_3W_256 * 6) - 3] = W4[(T_TM3_3W_256 << 1) - 1];
  
@@ -179,7 +264,12 @@
  	}
  
  	for (int32_t i = 0 ; i < 6 * T_TM3_3W_256 - 2 ; i++) {
-@@ -540,7 +539,7 @@
+-		uint64_t *out64 = Out + (i << 2);
++		uint64_t *out64 = ((uint64_t *)Out) + (i << 2);
+ 		_mm256_storeu_si256((__m256i *)out64, ro256[i]);
+ 	}
+ }
+@@ -540,7 +518,7 @@
   * @param[in] in Pointer to the polynomial A(x)
   * @param[in] size used to define the number of coeeficients of A
   */
@@ -188,16 +278,47 @@
  	out[0] = in[0];
  	for (int32_t i = 1 ; i < 2 * (size + 2) ; i++) {
  		out[i]= out[i - 1] ^ in[i];
-@@ -557,7 +556,7 @@
+@@ -557,7 +535,7 @@
   * @param[in] A Pointer to the polynomial A(x)
   * @param[in] B Pointer to the polynomial B(x)
   */
 -void TOOM3RecMult(uint64_t *Out, const uint64_t *A, const uint64_t *B) {
-+static void TOOM3RecMult(uint64_t *Out, const uint64_t *A, const uint64_t *B) {
++static void TOOM3RecMult(__m256i *Out, const uint64_t *A, const uint64_t *B) {
  	__m256i U0[T_TM3R_3W_256 + 2], V0[T_TM3R_3W_256 + 2], U1[T_TM3R_3W_256 + 2], V1[T_TM3R_3W_256 + 2], U2[T_TM3R_3W_256 + 2], V2[T_TM3R_3W_256 + 2];
  	__m256i W0[2 * (T_TM3R_3W_256 + 2)], W1[2 * (T_TM3R_3W_256 + 2)], W2[2 * (T_TM3R_3W_256 + 2)], W3[2 * (T_TM3R_3W_256 + 2)], W4[2 * (T_TM3R_3W_256 + 2)];
  	__m256i tmp[2 * (T_TM3R_3W_256 + 2) + 3];
-@@ -674,7 +673,7 @@
+@@ -600,7 +578,7 @@
+ 	}
+ 
+ 	//W1 = W2 * W3
+-	TOOM3Mult((uint64_t *) W1,(uint64_t *) W2,(uint64_t *) W3);
++	TOOM3Mult(W1,(uint64_t *) W2,(uint64_t *) W3);
+ 	//W0 =(U1 + U2*x)*x ; W4 =(V1 + V2*x)*x (SIZE = T_TM3_3W_256 + 2 !)
+ 	W0[0] = zero;
+ 	W4[0] = zero;
+@@ -628,17 +606,17 @@
+ 	}
+ 
+ 	//W3 = W3 * W2      ; W2 = W0 * W4
+-	TOOM3Mult((uint64_t *) tmp,(uint64_t *) W3,(uint64_t *) W2);
++	TOOM3Mult(tmp,(uint64_t *) W3,(uint64_t *) W2);
+ 
+ 	for (int32_t i = 0 ; i < 2 * (T_TM3R_3W_256 + 2) ; i++) {
+ 		W3[i] = tmp[i];
+     	}
+ 
+-	TOOM3Mult((uint64_t *) W2,(uint64_t *) W0,(uint64_t *) W4);
++	TOOM3Mult(W2,(uint64_t *) W0,(uint64_t *) W4);
+ 
+ 	//W4 = U2 * V2      ; W0 = U0 * V0
+-	TOOM3Mult((uint64_t *) W4,(uint64_t *) U2,(uint64_t *) V2);
+-	TOOM3Mult((uint64_t *) W0,(uint64_t *) U0,(uint64_t *) V0);
++	TOOM3Mult(W4,(uint64_t *) U2,(uint64_t *) V2);
++	TOOM3Mult(W0,(uint64_t *) U0,(uint64_t *) V0);
+ 
+ 	//Interpolation phase
+ 	//9 add, 1 shift, 1 Smul, 2 Sdiv (2n)
+@@ -674,7 +652,7 @@
  		tmp[i + 3] ^= W4[i];
      	}
  
@@ -206,7 +327,7 @@
  
  	//W3 =(W3 + W1)/(x*(x+1))
  	for (int32_t i = 0 ; i < 2 * (T_TM3R_3W_256 + 2) - 1 ; i++) {
-@@ -683,7 +682,7 @@
+@@ -683,7 +661,7 @@
     	 }
  
  	tmp[ 2 * (T_TM3R_3W_256 + 2) - 1] = zero;
@@ -215,4 +336,32 @@
  
  	//W1 = W1 + W4 + W2
  	for (int32_t i = 0 ; i < 2 * (T_TM3R_3W_256 + 2) ; i++) {
+@@ -720,7 +698,7 @@
+ 
+ 
+ 	for (int32_t i = 0 ; i < 6 * T_TM3R_3W_256 - 2 ; i++) {
+-		uint64_t *out64 = Out + (i << 2);
++		uint64_t *out64 = ((uint64_t *)Out) + (i << 2);
+ 		_mm256_storeu_si256((__m256i *)out64, ro256[i]);
+ 	}
+ }
+@@ -739,12 +717,12 @@
+  */
+ void vect_mul(uint64_t *o, const uint64_t *a1, const uint64_t *a2) {
+ 	TOOM3RecMult(a1_times_a2, a1, a2);
+-	reduce(o, a1_times_a2);
++	reduce(o, (uint64_t *)a1_times_a2);
+ 
+ 	// clear all
+-	#ifdef __STDC_LIB_EXT1__
+-		memset_s(a1_times_a2, 0, (VEC_N_SIZE_64 << 1) * sizeof(uint64_t));
+-	#else
+-		memset(a1_times_a2, 0, (VEC_N_SIZE_64 << 1) * sizeof(uint64_t));
+-	#endif
++#ifdef __STDC_LIB_EXT1__
++	memset_s(a1_times_a2, 0, (2 * VEC_N_SIZE_256 + 1) * sizeof(__m256i));
++#else
++	memset(a1_times_a2, 0, (2 * VEC_N_SIZE_256 + 1) * sizeof(__m256i));
++#endif
+ }
 
