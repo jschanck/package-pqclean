3abae9975eb72fc983b68a0981c08146a20a2cda
John M. Schanck <jschanck@uwaterloo.ca>
2020-09-14 12:57:00 -0400

    satisfy test_boolean in kem.c

--- hqc/patches/Optimized_Implementation_hqc-192_src_kem.c
+++ hqc/patches/Optimized_Implementation_hqc-192_src_kem.c
@@ -13,7 +13,16 @@
  	sha512(ss, mc, VEC_K_SIZE_BYTES + VEC_N_SIZE_BYTES + VEC_N1N2_SIZE_BYTES);
  
  	// Computing ciphertext
-@@ -140,9 +140,9 @@
+@@ -109,7 +109,7 @@
+ 		printf("\n\n\n\n### DECAPS ###");
+ 	#endif
+         
+-	int8_t result = -1;
++	uint8_t result;
+ 	uint64_t u[VEC_N_256_SIZE_64] = {0};
+ 	uint64_t v[VEC_N1N2_256_SIZE_64] = {0};
+ 	unsigned char d[SHA512_BYTES] = {0};
+@@ -140,17 +140,19 @@
  	sha512(d2, (unsigned char *) m, VEC_K_SIZE_BYTES);
  
  	// Computing shared secret
@@ -26,4 +35,24 @@
  	sha512(ss, mc, VEC_K_SIZE_BYTES + VEC_N_SIZE_BYTES + VEC_N1N2_SIZE_BYTES);
  
  	// Abort if c != c' or d != d'
+-	result = (vect_compare(u, u2, VEC_N_SIZE_BYTES) == 0 && vect_compare(v, v2, VEC_N1N2_SIZE_BYTES) == 0 && vect_compare((uint64_t *)d, (uint64_t *)d2, SHA512_BYTES) == 0);
++	result = vect_compare(u, u2, VEC_N_SIZE_BYTES);
++	result |= vect_compare(v, v2, VEC_N1N2_SIZE_BYTES);
++	result |= memcmp(d, d2, SHA512_BYTES);
++	result = (uint8_t) (-((int16_t) result) >> 15);
+ 	for (size_t i = 0 ; i < SHARED_SECRET_BYTES ; i++) {
+-		ss[i] = result * ss[i];
++		ss[i] &= ~result;
+ 	}
+-	result--;
+ 
+ 	#ifdef VERBOSE
+ 		printf("\n\npk: "); for(int i = 0 ; i < PUBLIC_KEY_BYTES ; ++i) printf("%02x", pk[i]);
+@@ -165,5 +167,5 @@
+ 		printf("\n\n# Checking Ciphertext - End #\n");
+ 	#endif
+ 
+-	return result;
++	return result&1;
+ }
 
--- hqc/patches/Reference_Implementation_hqc-rmrs-192_src_kem.c
+++ hqc/patches/Reference_Implementation_hqc-rmrs-192_src_kem.c
@@ -35,7 +35,13 @@
  	sha512(ss, mc, VEC_K_SIZE_BYTES + VEC_N_SIZE_BYTES + VEC_N1N2_SIZE_BYTES);
  
  	// Computing ciphertext
-@@ -114,7 +114,7 @@
+@@ -109,12 +109,12 @@
+         	printf("\n\n\n\n### DECAPS ###");
+ 	#endif
+ 
+-	int8_t result = -1;
++	uint8_t result;
+ 	uint64_t u[VEC_N_SIZE_64] = {0};
  	uint64_t v[VEC_N1N2_SIZE_64] = {0};
  	unsigned char d[SHA512_BYTES] = {0};
  	unsigned char pk[PUBLIC_KEY_BYTES] = {0};
@@ -44,7 +50,7 @@
  	uint8_t theta[SHA512_BYTES] = {0};
  	uint64_t u2[VEC_N_SIZE_64] = {0};
  	uint64_t v2[VEC_N1N2_SIZE_64] = {0};
-@@ -131,18 +131,18 @@
+@@ -131,26 +131,28 @@
  	hqc_pke_decrypt(m, u, v, sk);
  
  	// Computing theta
@@ -67,4 +73,24 @@
  	sha512(ss, mc, VEC_K_SIZE_BYTES + VEC_N_SIZE_BYTES + VEC_N1N2_SIZE_BYTES);
  
  	// Abort if c != c' or d != d'
+-	result = (vect_compare(u, u2, VEC_N_SIZE_BYTES) == 0 && vect_compare(v, v2, VEC_N1N2_SIZE_BYTES) == 0 && memcmp(d, d2, SHA512_BYTES) == 0);
++	result = vect_compare(u, u2, VEC_N_SIZE_BYTES);
++	result |= vect_compare(v, v2, VEC_N1N2_SIZE_BYTES);
++	result |= memcmp(d, d2, SHA512_BYTES);
++	result = (uint8_t) (-((int16_t) result) >> 15);
+ 	for (size_t i = 0 ; i < SHARED_SECRET_BYTES ; i++) {
+-		ss[i] = result * ss[i];
++		ss[i] &= ~result;
+ 	}
+-	result--;
+ 
+ 	#ifdef VERBOSE
+ 		printf("\n\npk: "); for(int i = 0 ; i < PUBLIC_KEY_BYTES ; ++i) printf("%02x", pk[i]);
+@@ -165,5 +167,5 @@
+ 		printf("\n\n# Checking Ciphertext - End #\n");
+ 	#endif
+ 
+-	return result;
++	return result&1;
+ }
 
