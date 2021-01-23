044f5c5345a78dd19ce678c99fd11552c69b1eef
John M. Schanck <jschanck@uwaterloo.ca>
2021-01-23 15:40:05 -0500

    Removing GeMSS

--- gemss/patches/Optimized_Implementation_sign_GeMSS128_src_rem_gf2n.c
+++
@@ -1,9 +0,0 @@
---- upstream/Optimized_Implementation/sign/GeMSS128/src/rem_gf2n.c
-+++ upstream-patched/Optimized_Implementation/sign/GeMSS128/src/rem_gf2n.c
-@@ -1,5 +1,4 @@
- #include "rem_gf2n.h"
--#include "rem_gf2x.h"
- #include "simd.h"
- 
- 
-
